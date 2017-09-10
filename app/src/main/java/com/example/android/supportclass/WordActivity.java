b8a6e82fdbdedb71ef1d23c74216acb41473337b
Jeff <fhwuse@gmail.com>
2017-09-10 21:41:46 +1200

    Get a word and its meaning in the xml file and then show them on the screen.

--- app/src/main/java/com/example/android/supportclass/WordActivity.java
+++ app/src/main/java/com/example/android/supportclass/WordActivity.java
@@ -5,11 +5,22 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import java.io.InputStream;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
 public class WordActivity extends AppCompatActivity {
     private TextView mTextMessage;
+    private TextView tvScreenWord;
+    private TextView tvScreenMeaning;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -42,8 +53,32 @@ public class WordActivity extends AppCompatActivity {
         setContentView(R.layout.activity_word);
 
         mTextMessage = (TextView) findViewById(R.id.message);
+        tvScreenWord = (TextView) findViewById(R.id.txtWord);
+        tvScreenMeaning = (TextView) findViewById(R.id.txtMeaning);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setSelectedItemId(R.id.navigation_word);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        openWordCards();
+    }
+
+    private void openWordCards() {
+        InputStream input = getResources().openRawResource(R.raw.vocabularies);
+
+        try{
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(input);
+            doc.getDocumentElement().normalize();
+            NodeList nList = doc.getElementsByTagName("word");
+            Element element = (Element)nList.item(0);
+            String screenWord = element.getElementsByTagName("english").item(0).getTextContent();
+            String screenWordMeaning = element.getElementsByTagName("meaning").item(0).getTextContent();
+            tvScreenWord.setText(screenWord);
+            tvScreenMeaning.setText(screenWordMeaning);
+        }catch(Exception ex){
+            Log.i("ExceptionOccurred", "openWordCards: load xml file exception. Msg: " + ex.getMessage());
+        }
+
     }
 }
